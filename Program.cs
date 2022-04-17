91f29fa331b64296640252805690f3f0c8fd12ea
Vetrov Yury <888vetrov@gmail.com>
2022-04-17 18:57:56 +0300

    3 Task

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 3: Напишите программу, которая выводит третью
+// цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+string number = Console.ReadLine();
+int length = number.Length;
+if (number.Length <= 1)
+    Console.WriteLine("Нет второй цифры");
+else
+    Console.WriteLine(number[1]);
+//if (number < 0)
+   // (number = -number);
+   // {
+   //     Console.WriteLine("введите положительное число");
+   // }
+//string number = Console.ReadLine();
+//Console.WriteLine(number[2]);
+
+//if (number.Length() <= 0)
+    ///Console.WriteLine("Число состоит из одного числа, введите трехзначное число");
+//else
+    //Console.WriteLine(number[2]);
+
+//if (number.Length == 1)
+//Console.WriteLine("Нет второй цифры");
+        //while (num >= 1000)
+        //{
+            //num = num / 10;
+           // num /=10;
+        //}
+        //Console.WriteLine(num % 10);
